191a27e0b9e2c005ee8db5afac3c546a3b946a68
orchief <orchief@163.com>
2021-03-10 00:13:55 +0800

    feat: 新增demo

---
+++ DeamonDemo.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once "./vendor/autoload.php";
+
+use think\facade\Db;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Tools\Daemon;
+
+class DeamonDemo extends Daemon
+{
+    /**
+     * 守护进程的任务
+     */
+    protected function job()
+    {
+        // TODO 常驻脚本代码
+        while (true) {
+            $this->log('job done : ' . date('Y-m-d H:i:s') . PHP_EOL);
+            sleep(10);
+        }
+    }
+
+    /**
+     * 自动日志
+     *
+     * @return void
+     */
+    private function log($msg)
+    {
+        // create a log channel
+        $log = new Logger('report');
+        $log->pushHandler(new StreamHandler(dirname(__FILE__) . '/cache/report.log'));
+        $log->info($msg);
+    }
+}
+
+$deamon = new DeamonDemo(dirname(__FILE__) . '/cache/deamondemo.log');
+$deamon->run($argv);
